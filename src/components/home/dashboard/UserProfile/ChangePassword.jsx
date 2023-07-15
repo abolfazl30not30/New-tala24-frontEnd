cd5c40d76f2d0878dbc18c2313359c272c37b56c
Abolfazl <abolfazl30not30@gmail.com>
2023-07-15 23:01:23 +0330

    edit change password

--- src/components/home/dashboard/UserProfile/ChangePassword.jsx
+++ src/components/home/dashboard/UserProfile/ChangePassword.jsx
@@ -93,6 +93,7 @@ const ChangePassword = () =>{
                 setNewPassword("");
                 setRepeatNewPassword("");
                 setOTPCode("")
+                setIsSendOTP(false)
             }
 
         }
